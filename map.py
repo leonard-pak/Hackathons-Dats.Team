26a4b0b7e7c4e8b8d609099297a4085adf588fa1
Leonard Pak <leopak2000@gmail.com>
2024-07-13 02:33:57 +0300

    nightly commit

--- map.py
+++ map.py
@@ -1,18 +1,45 @@
 from store import Store
 import numpy as np
 import numpy.typing as npt
+from dataclasses import dataclass
+import enum
+import copy
+
+
+class PointType(enum.Enum):
+    EMPTY = 0
+    ZOMBIE = 1
+    ZOMBIE_SPAWN = 2
+    WALL = 3
+    MY_BASE = 4
+    MY_CAPITAL = 5
+    ENEMY_BASE = 6
+    ENEMY_CAPITAL = 7
 
 
 class Map():
-    def __init__(self, store: Store) -> None:
+    def __init__(self, store: Store, reserve_multiplier: int) -> None:
         self._store = store
         self._zombie_spots = self._store.get_spots()
         self._walls = self._store.get_walls()
+
+        self._static_map, self._map_shift = self._build_static_map(
+            reserve_multiplier=reserve_multiplier)
+
         self.update()
         self._init_point = self.get_base_center()
 
     def update(self):
         self._base = self._store.get_base()
+        # TODO Дополучать всё остальное на карту
+
+        self._map = copy.deepcopy(self._static_map)
+        for block_id, block_info in self._base.blocks.items():
+            self._add_to_map(block_info.point[0], block_info.point[1],
+                             PointType.MY_CAPITAL if block_id == self._base.head else PointType.MY_BASE)
+
+    def _add_to_map(self, x: int, y: int, info: PointType):
+        self._map[x + self._map_shift][y + self._map_shift] = info
 
     def get_base_center(self) -> npt.NDArray[np.int32]:
         return np.array([block.point for _, block in self._base.blocks.items()]).mean(axis=1)
@@ -21,3 +48,29 @@ class Map():
         nearest_idx = np.array([np.abs(spot.point - self._init_point).sum()
                                 for spot in self._zombie_spots]).argmin()
         return self._zombie_spots[nearest_idx]
+
+    def is_point_available(self, x: int, y: int):
+        # TODO проверять в соотвествие с правилами
+        pass
+
+    def _build_static_map(self, reserve_multiplier: int):
+        points = list[npt.NDArray[np.int32]]()
+        types = list[PointType]()
+        for spot in self._zombie_spots:
+            points.append(spot.point)
+            types.append(PointType.ZOMBIE_SPAWN)
+        for wall in self._walls:
+            points.append(wall.point)
+            types.append(PointType.WALL)
+        points = np.array(points)
+        max_x_y: npt.NDArray[np.int32] = points.max(axis=0)
+        min_x_y: npt.NDArray[np.int32] = points.min(axis=0)
+        scales = (max_x_y - min_x_y) * reserve_multiplier
+        map_idx_shifts = (scales - max_x_y - min_x_y) / 2
+        # TODO В какую сторону лучше округлять? Уже засыпаю
+        map_idx_shifts = np.round(map_idx_shifts).astype(int)
+        static_map = np.zeros((scales[0], scales[1]))
+        for point_coord, point_type in zip(points, types):
+            idxes = point_coord + map_idx_shifts
+            static_map[idxes[0]][idxes[1]] = point_type
+        return static_map, map_idx_shifts
--- visualize.py
+++ visualize.py
@@ -4,17 +4,7 @@ import matplotlib.colors as mpl_col
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-# Определяем разные типы объектов на карте
-class MapNumbers(int, enum.Enum):
-    EMPTY = 0
-    ZOMBIE = 1
-    ZOMBIE_SPAWN = 2
-    WALL = 3
-    MY_BASE = 4
-    MY_CAPITAL = 5
-    ENEMY_BASE = 6
-    ENEMY_CAPITAL = 7
+from map import PointType
 
 
 class MapColors(str, enum.Enum):
@@ -39,7 +29,8 @@ cmap = mpl_col.ListedColormap([color.value for color in MapColors])
 def visualize_map(game_map: np.ndarray):
     x_size, y_size = game_map.shape
 
-    plt.imshow(game_map, origin='lower', extent=(0, x_size, 0, y_size), vmin=0, vmax=len(cmap.colors), cmap=cmap)
+    plt.imshow(game_map, origin='lower', extent=(
+        0, x_size, 0, y_size), vmin=0, vmax=len(cmap.colors), cmap=cmap)
 
     # Добавление сетки клеток
     plt.grid(which='both', color='black', linestyle='-', linewidth=1)
@@ -61,16 +52,17 @@ def _main():
     game_map = np.zeros((grid_size, grid_size))
 
     def fill_area(map_array, top_left, bottom_right, value):
-        map_array[top_left[0]:bottom_right[0], top_left[1]:bottom_right[1]] = value
+        map_array[top_left[0]:bottom_right[0],
+                  top_left[1]:bottom_right[1]] = value
 
     # Заполняем карту объектами
-    fill_area(game_map, (2, 2), (5, 5), MapNumbers.ZOMBIE.value)
-    fill_area(game_map, (6, 6), (10, 10), MapNumbers.WALL.value)
-    fill_area(game_map, (12, 2), (15, 5), MapNumbers.MY_BASE.value)
-    game_map[13, 3] = MapNumbers.MY_CAPITAL.value
-    fill_area(game_map, (2, 12), (5, 15), MapNumbers.ENEMY_BASE.value)
-    game_map[3, 13] = MapNumbers.ENEMY_CAPITAL.value
-    game_map[8, 8] = MapNumbers.ZOMBIE_SPAWN.value
+    fill_area(game_map, (2, 2), (5, 5), PointType.ZOMBIE.value)
+    fill_area(game_map, (6, 6), (10, 10), PointType.WALL.value)
+    fill_area(game_map, (12, 2), (15, 5), PointType.MY_BASE.value)
+    game_map[13, 3] = PointType.MY_CAPITAL.value
+    fill_area(game_map, (2, 12), (5, 15), PointType.ENEMY_BASE.value)
+    game_map[3, 13] = PointType.ENEMY_CAPITAL.value
+    game_map[8, 8] = PointType.ZOMBIE_SPAWN.value
 
     visualize_map(game_map)
 
