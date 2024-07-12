9be9661b1729865b5f9c0e9514c4effc8c7172ac
GiveMeTheReason <botva2000@gmail.com>
2024-07-12 22:04:19 +0300

    add loop fetch

--- scripts/send_request.py
+++ scripts/send_request.py
@@ -1,23 +1,44 @@
 import json
+import time
 
 import client
 
 
-def send_request(client: client.Client):
+def log_request(result, op_type = ''):
+    now_ts = int(time.time())
+    print(result)
 
-    # return client.put_participate()
+    with open(f'test_log/response_{op_type}_{now_ts}.json', 'w') as file:
+        json.dump(result, file)
 
-    # return client.get_world()
 
-    return client.get_units()
+def send_request(game_client: client.Client):
 
-    # return client.get_rounds()
+    # return game_client.put_participate()
+
+    # return game_client.get_world()
+
+    return game_client.get_units()
+
+    # return game_client.get_rounds()
+
+
+    return game_client.post_commands(attack, build, move_base)
+
+
+def loop_fetch(game_client: client.Client):
+    log_request(game_client.get_rounds(), 'get_rounds')
+
+    log_request(game_client.put_participate(), 'put_participate')
+
+    log_request(game_client.get_world(), 'get_world')
+
+    while True:
+        log_request(game_client.get_units(), 'get_units')
+        time.sleep(2)
 
 
 if __name__ == '__main__':
     game_client = client.Client()
-    result = send_request(game_client)
-    print(result)
 
-    with open('response.json', 'w') as file:
-        json.dump(result, file)
+    loop_fetch(game_client)
