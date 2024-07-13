a95d053cf6bb5c7f05eb8bfb355ac22efa527af7
GiveMeTheReason <botva2000@gmail.com>
2024-07-13 12:24:07 +0300

    add attacker

--- strategy/attacker.py
+++ strategy/attacker.py
@@ -1,7 +1,10 @@
 import typing as tp
 
+import numpy as np
+
 import map_lib
 import models
+import utils
 
 
 def _add_attack_target(attack_targets: tp.Dict, target_pos: tp.Tuple[int, int], target_health: int):
@@ -35,14 +38,36 @@ def _attack_targets(game_map: map_lib.Map) -> tp.Dict[tp.Tuple[int, int], tp.Tup
 def get_attack(game_map: map_lib.Map) -> tp.List[models.Attack]:
     attack_targets = _attack_targets(game_map)
 
-    # [target_count, max_hp, x, y]
-    targets_sorted_list = ...
-
-    game_map._base
+    # (target_count, max_hp, x, y)
+    targets_sorted = sorted([(-stats[0], stats[1], point[0], point[1]) for point, stats in attack_targets.items()])
 
     attack_command: tp.List[models.Attack] = []
 
     for block_id, block in game_map._base.blocks.items():
-        block.distance
+        selected_target = None
+        selected_target_idx = None
+
+        distance = block.distance
+        for idx, target in enumerate(targets_sorted):
+            target_distance = utils.calc_range(block.point, np.array([target[-2], target[-1]]))
+            if target_distance <= distance:
+                selected_target = target
+                selected_target_idx = idx
+                break
+
+        if not selected_target or not selected_target_idx:
+            continue
+
+        damage = block.attack
+        remaining_hp = selected_target[1] - damage
+        target_x = selected_target[-2]
+        target_y = selected_target[-1]
+        if remaining_hp <= 0:
+            targets_sorted.pop(selected_target_idx)
+        else:
+            targets_sorted[selected_target_idx] = (selected_target[0], remaining_hp, target_x, target_y)
+            targets_sorted.sort()
+
+        attack_command.append(models.Attack(block_id, models.Point(target_x, target_y)))
 
     return attack_command
